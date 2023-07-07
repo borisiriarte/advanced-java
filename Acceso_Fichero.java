8c9106f646ecd99ed4e505df3cce25c724c8630b
Boris2514 <borismiashiro@gmail.com>
2023-07-06 21:09:33 -0400

    Updating Doc

--- Acceso_Fichero.java
+++ Acceso_Fichero.java
@@ -1,21 +1,39 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Acceso_Fichero {
 
     public static void main(String[] args) {
         //streams or data flow
+        Leer_Fichero accessed = new Leer_Fichero();
 
+
+        accessed.read_file();
     }
 }
 
 class Leer_Fichero {
-    public void read() {
+    public void read_file() {
         try {
-            FileReader input = new FileReader("/example.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("El archivo ha sido movido o no existe. Error: " + e );
+            FileReader input = new FileReader("./example.txt");
+
+            int redFile = 0;
+
+            StringBuilder fullText = new StringBuilder();
+
+            while(redFile != -1) {
+                redFile = input.read();
+
+                char letter = (char) redFile;
+
+                fullText.append(letter);
+            }
+
+            System.out.println(fullText);
+
+            input.close();
+        } catch (IOException e) {
+            System.out.println("El archivo ha sido movido o no existe.\nError: " + e );
         }
     }
 }
---
+++ Escribiendo_Fichero.java
@@ -0,0 +1,2 @@
+public class Escribiendo_Fichero {
+}
